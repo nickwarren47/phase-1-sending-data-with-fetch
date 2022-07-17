a25f49be75b13af37e0461fda9abe15499af9dc4
Nick Warren <nickwarren47@gmail.com>
2022-07-16 22:11:18 -0600

    commit tree 7e1a4dc140c3211a778593adabad7e5a0f7d31a7

--- index.js
+++ index.js
@@ -1,1 +1,98 @@
 // Add your code here
+
+// const { bodyParser } = require("json-server");
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify({
+//       dogName: "Byron",
+//       dogBreed: "Poodle",
+//     }),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject);
+
+
+// another method for getting the same result
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+  
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject);
+
+
+
+// another method that will also provide the same results
+
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json",
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   }),
+// });
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+  
+//   // method: "POST" is missing from the object below
+//   const configurationObject = {
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     })
+//     .catch(function (error) {
+//       alert("Bad things! Ragnarők!");
+//       console.log(error.message);
+//     });
+
+const submitData = {
+    name: 'Nick',
+    email: 'scuba@gmail.com',
+};
+fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+        'Content-Type':'application/json',
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(submitData)
+})
+    .then (res => res.json())
+    .then (data => console.log(data))
+    .catch(function (error) {
+        alert('Houston, we have a problem!');
+        console.log(error.message);
+    });
+    
+  
